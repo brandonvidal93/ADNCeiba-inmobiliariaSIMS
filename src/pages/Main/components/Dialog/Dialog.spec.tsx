9dcfc2356297918ab52361daf1a5d47596f461a9
CEIBA\brandon.vidal <brandon.vidal@ceiba.com.co>
2021-08-30 18:43:23 -0500

    A - Diagol MUI test

---
+++ src/pages/Main/components/Dialog/Dialog.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Dialog } from '@material-ui/core';
+import { render, within } from '@testing-library/react';
+
+const DialogTest = () => {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <button onClick={handleClickOpen}>Open dialog</button>
+      <Dialog data-testid="dialog-testid" open={open} onClose={handleClose}>
+        <div data-testid="inside-testid">Dialog content</div>
+      </Dialog>
+    </>
+  );
+}
+
+test('Dialog', () => {
+  const { getByTestId } = render(<DialogTest />);
+
+  expect(getByTestId('inside-testid')).toHaveTextContent('Dialog content');
+});
