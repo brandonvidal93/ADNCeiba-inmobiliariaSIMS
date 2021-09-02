7500cbc5161a20cdc9f1abb391657cb3ef662ec7
CEIBA\brandon.vidal <brandon.vidal@ceiba.com.co>
2021-09-02 11:17:57 -0500

    U - Smell code

--- cypress/integration/flujo-editar-propiedad.spec.js
+++ cypress/integration/flujo-editar-propiedad.spec.js
@@ -8,9 +8,9 @@ describe('Flujo para editar una propiedad', () => {
     cy.wait(3000);
     cy.get('#ubication').click();
     cy.wait(3000);
-    cy.get('[data-value="0.1_1_1 - Popular"]').click();
+    cy.get('[data-value="0.1_2_2 - Santa Cruz"]').click();
     cy.wait(3000);
-    cy.get('#office').clear().type(1);
+    cy.get('#office').clear().type(3);
     cy.wait(3000);
     cy.get('#btnSave').click();
   });
