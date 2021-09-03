1da38799d2c0adae8fa237af56cfaf96a5b3019b
CEIBA\brandon.vidal <brandon.vidal@ceiba.com.com>
2021-09-03 11:09:25 -0500

    U - Integration Test Edit Building

--- cypress/integration/flujo-editar-propiedad.spec.js
+++ cypress/integration/flujo-editar-propiedad.spec.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-/// <reference types="Cypress"/>
-
-describe('Flujo para editar una propiedad', () => {
-  it('Editar registro existoso', () => {
-    cy.visit('http://localhost:3000');
-    cy.wait(3000);
-    cy.get('#btnEdit-1').click();
-    cy.wait(3000);
-    cy.get('#ubication').click();
-    cy.wait(3000);
-    cy.get('[data-value="0.1_1_1 - Popular"]').click();
-    cy.wait(3000);
-    cy.get('#office').clear().type(1);
-    cy.wait(3000);
-    cy.get('#btnSave').click();
-  });
-});
-=======
 /// <reference types="Cypress"/>
 
 describe('Flujo para editar una propiedad', () => {
@@ -34,4 +15,3 @@ describe('Flujo para editar una propiedad', () => {
     cy.get('#btnSave').click();
   });
 });
->>>>>>> develop
